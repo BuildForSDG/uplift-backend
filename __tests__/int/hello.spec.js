77f75c3669afb260846534c66592a538a6dc103b
Victor Baraka <barakavictor62@gmail.com>
2020-05-23 15:48:18 +0300

    chore(linting): fix linting issues

--- __tests__/int/hello.spec.js
+++ __tests__/int/hello.spec.js
@@ -1,15 +1,14 @@
-const request = require("supertest");
-const createContainer = require("../../src/bootstrap");
+const request = require('supertest');
+const createContainer = require('../../src/bootstrap');
 
-describe("GET /", () => {
-    
-    it("should return 'Hello from uplift bare skeleton backend'", async () => {
-        const container = createContainer();
-        const app = container.App;
-        const response = await request(app)
-        .get("/");
-        expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty("message");
-        expect(response.body.message).toEqual("Hello from uplift bare skeleton backend");
-    });
+describe('GET /', () => {
+  it("should return 'Hello from uplift bare skeleton backend'", async () => {
+    const container = createContainer();
+    const app = container.App;
+    const response = await request(app)
+      .get('/');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toEqual('Hello from uplift bare skeleton backend');
+  });
 });
